a40708da0d08de7dd2b8fc02af3d2d41624bb6e6
Spencer Robertson <spencer.robertson@zirconsoftware.co.uk>
2019-03-04 14:24:23 +0000

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const API_PORT = 3001;
+const app = express();
+const router = express.Router();
+const request = require('request');
+const https = require("https");
+const api = require("./api");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api", api);
+
+app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
